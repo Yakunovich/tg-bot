c4b29e1c3f28d2c1b1f46ae870de68ad52ff97a3
Yakunovich <54987759+Yakunovich@users.noreply.github.com>
2023-06-13 19:50:37 +0300

    Go back/forward buttons on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React from "react";
 import Button from "../Button/Button";
-import {useTelegram} from "../../hooks/useTelegram";
-import './Header.css';
-import { Link } from 'react-router-dom';
+import { useTelegram } from "../../hooks/useTelegram";
+import "./Header.css";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    const {user, onClose} = useTelegram();
+  const { user, onClose } = useTelegram();
+  const navigate = useNavigate();
 
-    return (
-        <div className={'header'}>
-            <Button onClick={onClose}>Закрыть</Button>
-            <span className={'username'}>
-                {user?.username}
-            </span>
-            <div><Link to='/form'>Form</Link></div>
-        </div>
-    );
+  return (
+    <div className={"header"}>
+      <Button onClick={onClose}>Закрыть</Button>
+      <Button onClick={() => navigate(-1)}>Go back</Button>
+      <Button onClick={() => navigate(1)}>Go forward</Button>
+      <span className={"username"}>{user?.username}</span>
+      <div>
+        <Link to="/form">Form</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
